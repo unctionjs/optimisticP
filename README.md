426cac976d3ab8496ab74284bdff7bfa154da422
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-07 14:39:21 -0700

    Copying over

--- README.md
+++ README.md
@@ -1,17 +1,15 @@
-# @unction/tempLate
+# @unction/optimisticP
+
 
 ![Tests][BADGE_TRAVIS]
 ![Stability][BADGE_STABILITY]
 ![Dependencies][BADGE_DEPENDENCY]
 
-> mixed -> mixed
+> Array<any | Promise<any>> -> Promise<Array<any>>
 
-An example function.
+Will take an array of promises and returns a promise of only the resolved promises.
 
-``` javascript
-tempLate(1) // 1
-```
+[BADGE_TRAVIS]: https://img.shields.io/travis/unctionjs/optimisticP.svg?maxAge=2592000&style=flat-square
 
-[BADGE_TRAVIS]: https://img.shields.io/travis/unctionjs/tempLate.svg?maxAge=2592000&style=flat-square
 [BADGE_STABILITY]: https://img.shields.io/badge/stability-strong-green.svg?maxAge=2592000&style=flat-square
-[BADGE_DEPENDENCY]: https://img.shields.io/david/unctionjs/tempLate.svg?maxAge=2592000&style=flat-square
+[BADGE_DEPENDENCY]: https://img.shields.io/david/unctionjs/optimisticP.svg?maxAge=2592000&style=flat-square
--- test.js
+++ test.js
@@ -1,13 +1,44 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import tempLate from "./"
+import optimisticP from "./"
 
-test(({same, end}) => {
-  same(
-    tempLate(true),
-    false
+test(({includes}) => {
+  return optimisticP(
+    [
+      Promise.resolve("a")
+    ]
   )
+    .then((values) => includes(values, ["a"]))
+})
 
-  end()
+test(({includes}) => {
+  return optimisticP(
+    [
+      Promise.resolve("a"),
+      Promise.reject(new Error("b"))
+    ]
+  )
+    .then((values) => includes(values, ["a"]))
+})
+
+test(({includes}) => {
+  return optimisticP(
+    [
+      Promise.resolve("a"),
+      Promise.resolve("c")
+    ]
+  )
+    .then((values) => includes(values, ["a", "c"]))
+})
+
+test(({includes}) => {
+  return optimisticP(
+    [
+      Promise.resolve("a"),
+      Promise.reject(new Error("b")),
+      Promise.resolve("c")
+    ]
+  )
+    .then((values) => includes(values, ["a", "c"]))
 })
