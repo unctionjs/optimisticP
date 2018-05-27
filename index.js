8fcadae917473ef55bf4363fe2c19b6713d6a9a4
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:29 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-import mapValues from "@unction/mapvalues"
-import recordfrom from "@unction/recordfrom"
-import pipe from "@unction/pipe"
-import key from "@unction/key"
-import {has} from "ramda"
-import {reject} from "ramda"
-import thenCatchP from "@unction/thencatchp"
-import thenP from "@unction/thenp"
-import allP from "@unction/allp"
-
-const asResolved = recordfrom(["resolved"])
-const asRejected = recordfrom(["rejected"])
-const onlyResolved = reject(has("rejected"))
-const resolvedValues = mapValues(key("resolved"))
-const onlyResolvedValues = pipe([onlyResolved, resolvedValues])
-
-export default function optimisticP (promises: Array<mixed | Promise<mixed>>): Promise<Array<mixed>> {
-  return pipe([
-    mapValues(thenCatchP(asResolved)(asRejected)),
-    allP,
-    thenP(onlyResolvedValues),
-  ])(promises)
+import mapValues from "@unction/mapvalues";
+import recordfrom from "@unction/recordfrom";
+import pipe from "@unction/pipe";
+import key from "@unction/key";
+import { has } from "ramda";
+import { reject } from "ramda";
+import thenCatchP from "@unction/thencatchp";
+import thenP from "@unction/thenp";
+import allP from "@unction/allp";
+const asResolved = recordfrom(["resolved"]);
+const asRejected = recordfrom(["rejected"]);
+const onlyResolved = reject(has("rejected"));
+const resolvedValues = mapValues(key("resolved"));
+const onlyResolvedValues = pipe([onlyResolved, resolvedValues]);
+export default function optimisticP(promises) {
+  return pipe([mapValues(thenCatchP(asResolved)(asRejected)), allP, thenP(onlyResolvedValues)])(promises);
 }
