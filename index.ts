3cd16e000daa01d8dcaf6d9ebfc5bbdd654f9549
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 01:17:26 -0700

    Annotating, fixing issues with packages

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ import rejectByValue from "@unction/rejectbyvalue";
 import thenCatchP from "@unction/thencatchp";
 import thenP from "@unction/thenp";
 import allP from "@unction/allp";
+import {ListType} from "./types";
 
 const asResolved = objectFrom(["resolved"]);
 const asRejected = objectFrom(["rejected"]);
@@ -14,6 +15,12 @@ const onlyResolved = rejectByValue(has("rejected"));
 const resolvedValues = mapValues(get("resolved"));
 const onlyResolvedValues = pipe([onlyResolved, resolvedValues]);
 
-export default function optimisticP (promises) {
-  return pipe([mapValues(thenCatchP(asResolved)(asRejected)), allP, thenP(onlyResolvedValues)])(promises);
+export default function optimisticP<A> (promises: ListType<Promise<A>>): Promise<ListType<A>> {
+  return pipe([
+    mapValues(thenCatchP(asResolved)(asRejected)),
+    allP,
+    thenP(onlyResolvedValues),
+  ])(
+    promises
+  );
 }
