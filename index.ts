b741d8440e5a44562c275eb7015bff36b748b821
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-28 18:50:21 -0800

    Denormalizing some types to make things a little bit easier

--- index.ts
+++ index.ts
@@ -7,7 +7,6 @@ import rejectByValue from "@unction/rejectbyvalue";
 import thenCatchP from "@unction/thencatchp";
 import thenP from "@unction/thenp";
 import allP from "@unction/allp";
-import {ListType} from "./types";
 
 const asResolved = objectFrom(["resolved"]);
 const asRejected = objectFrom(["rejected"]);
@@ -15,7 +14,7 @@ const onlyResolved = rejectByValue(has("rejected"));
 const resolvedValues = mapValues(get("resolved"));
 const onlyResolvedValues = pipe([onlyResolved, resolvedValues]);
 
-export default function optimisticP<A> (promises: ListType<Promise<A>>): Promise<ListType<A>> {
+export default function optimisticP<A> (promises: ListType<Promise<A>>): Promise<Array<A> | Set<A>> {
   return pipe([
     mapValues(thenCatchP(asResolved)(asRejected)),
     allP,
